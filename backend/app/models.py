2811f6601bcdf2f10dfde88d779b90859dfe2f83
CasianCaraconcea <justicebreaker@yahoo.com>
2025-10-14 20:33:27 +0300

    Tabele + Migratii

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=255)
+    expected = models.CharField(max_length=255)
+    submitted = models.CharField(max_length=255)
+    was_correct = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.question} = {self.submitted} ({'✓' if self.was_correct else '✗'})"
+
+
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
