2eda7a1e4126157a5dc0497ceaae1c318e9f4e9c
Abel <abelcaluseri@yahoo.com>
2025-10-14 20:11:03 +0300

    project setup

---
+++ backend/PBMate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PBMate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PBMate.settings')
+
+application = get_asgi_application()
