6d39417e0cf03f9d5e25eae368c6355f7200b2ee
CasianCaraconcea <justicebreaker@yahoo.com>
2025-10-14 20:47:01 +0300

    Admin tabels

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Problem, User
 
 # Register your models here.
+
+def register(model_class):
+    admin.site.register(model_class)
+
+register(Problem)
+register(User)
